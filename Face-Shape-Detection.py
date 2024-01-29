adaf3fd9432458bb00eb35930fc68ace2341ba98
Praneethpshetty24 <praneeth666s@gmail.com>
2024-01-30 00:03:33 +0530

    I have forked

--- Face-Shape-Detection.py
+++ Face-Shape-Detection.py
@@ -9,7 +9,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 2.1, 4)
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2) 
 
 cv2.imshow('img', img)
 cv2.waitKey()
